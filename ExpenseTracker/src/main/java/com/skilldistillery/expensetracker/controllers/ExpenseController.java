6a80fd98400716e326901d6dab445e1091dc1ce4
Jerry Rogers <jerryrogers@Students-MacBook-Pro.local>
2019-12-13 15:45:22 -0700

    angular intitial stub

--- ExpenseTracker/src/main/java/com/skilldistillery/expensetracker/controllers/ExpenseController.java
+++ ExpenseTracker/src/main/java/com/skilldistillery/expensetracker/controllers/ExpenseController.java
@@ -2,10 +2,10 @@ package com.skilldistillery.expensetracker.controllers;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +20,7 @@ import com.skilldistillery.expensetracker.services.ExpenseService;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin({ "*", "http://localhost:4204" })
 public class ExpenseController {
 
 	@Autowired
